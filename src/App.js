8437789b86a52e50186eade1b88a0032b16f215e
matipeco <matiaspecorale1995@gmail.com>
2023-08-24 11:26:49 -0300

    asda

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Fade } from "react-awesome-reveal";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Fade cascade>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </Fade>
     </div>
   );
 }
